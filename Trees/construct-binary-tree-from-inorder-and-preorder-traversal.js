208524eaaab23cd5b9d0b8dc9bdf23d741f50122
Sava Stanchev <s.stanchev94@gmail.com>
2023-09-09 16:31:14 +0300

    construct binary tree from inorder and preorder traversal

---
+++ Trees/construct-binary-tree-from-inorder-and-preorder-traversal.js
@@ -0,0 +1,24 @@
+/**
+ * https://leetcode.com/problems/construct-binary-tree-from-preorder-and-inorder-traversal/
+ *
+ * Time O(n^2) | Space(n)
+ *
+ * first element in pre-order is root,
+ * elements left of root in in-order are left subtree,
+ * right of root are right subtree,
+ * recursively build subtrees
+ *
+ * @param {number[]} preorder
+ * @param {number[]} inorder
+ * @return {TreeNode}
+ */
+var buildTree = function (preorder, inorder) {
+  if (!preorder.length || !inorder.length) return null;
+
+  let root = new TreeNode(preorder[0]);
+  let mid = inorder.indexOf(preorder[0]);
+  root.left = buildTree(preorder.slice(1, mid + 1), inorder.slice(0, mid));
+  root.right = buildTree(preorder.slice(mid + 1), inorder.slice(mid + 1));
+
+  return root;
+};
