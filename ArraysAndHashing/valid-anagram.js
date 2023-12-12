3c8ff96f4e7a116ca426a98fba0a63b2fbf137ba
Sava Stanchev <s.stanchev94@gmail.com>
2023-12-12 13:40:28 +0200

    valid anagram

---
+++ ArraysAndHashing/valid-anagram.js
@@ -0,0 +1,37 @@
+/**
+ * https://leetcode.com/problems/valid-anagram/
+ *
+ * Time O(n) | Space O(1)
+ *
+ * hashmap to count each char in str1,
+ * decrement for str2
+ *
+ * @param {string} s
+ * @param {string} t
+ * @return {boolean}
+ */
+var isAnagram = function (s, t) {
+  if (s.length !== t.length) return false;
+
+  let map = new Map();
+
+  for (const char of s) {
+    const count = (map.get(char) || 0) + 1;
+    map.set(char, count);
+  }
+
+  for (const char of t) {
+    if (map.has(char)) {
+      const count = map.get(char) - 1;
+      map.set(char, count);
+
+      if (map.get(char) === 0) {
+        map.delete(char);
+      }
+    } else {
+      return false;
+    }
+  }
+
+  return true;
+};
